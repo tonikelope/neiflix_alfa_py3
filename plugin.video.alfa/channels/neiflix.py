1ae154cbfd888ffc6d497f38cb550b40657fb042
tonikelope <tonikelope@gmail.com>
2022-12-14 00:58:01 +0100

    Revert "2.10"
    This reverts commit aa6b5473a44875e997c40db84c823eca7b16cacb.

--- plugin.video.alfa/channels/neiflix.py
+++ plugin.video.alfa/channels/neiflix.py
@@ -25,7 +25,7 @@ from collections import OrderedDict
 
 CHECK_MEGA_STUFF_INTEGRITY = True
 
-NEIFLIX_VERSION = "2.10"
+NEIFLIX_VERSION = "2.9"
 
 NEIFLIX_LOGIN = config.get_setting("neiflix_user", "neiflix")
 
